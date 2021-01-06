dd3b15432dd4f37926f4fad036f06e85a79876f7
JoseLuis1611 <jose.artica@unmsm.edu.pe>
2021-01-06 13:14:16 -0500

    ffff

--- src/Configuracion/Conf.js
+++ src/Configuracion/Conf.js
@@ -1,5 +1,5 @@
 
-   const CONFIG1= 'http://localhost:8081/'
+   const CONFIG1= 'https://consultarecibos-back-end.herokuapp.com/'
    //const CONFIG1= 'https://sigapdev-conrecibospre-back.herokuapp.com'   
    //const CONFIG= 'https://sigapdev2-consultarecibos-back.herokuapp.com/'   
 
