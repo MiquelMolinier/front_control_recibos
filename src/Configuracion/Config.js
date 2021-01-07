7cecc2e09b6b82c68a62d0b8e515c03dfadd3b17
miquel <miquelmolinier@gmail.com>
2021-01-07 10:43:16 -0500

    final

--- src/Configuracion/Config.js
+++ src/Configuracion/Config.js
@@ -1,4 +1,4 @@
 
-   const CONFIG= 'http://localhost:8081/'
+   const CONFIG= 'https://consultarecibos-back-end.herokuapp.com/'
    //const CONFIG= 'https://sigapdev2-consultarecibos-back.herokuapp.com/'   
 export default CONFIG;
